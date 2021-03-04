7f4ec1efaded99c1add506059166d501e6cc90d5
VodimShilin <shilin.vv@phystech.edu>
2021-03-04 22:57:34 +0300

    fix TaskE

--- Hash-Contest/TaskE/TaskE.cpp
+++ Hash-Contest/TaskE/TaskE.cpp
@@ -2,27 +2,25 @@
 #include <string>
 #include <vector>
 
-long long length = 1'000'000;
-std::vector<char> letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+#define LENGTH 1'000'000
 
 int hash(const std::string& s, std::vector<std::pair<std::string, std::string>>& table, bool tomb) {
 	long long pos = 0;
 	for (size_t i = 0; i < s.size(); ++i) {
-		long long j = 0;
-		while (j < 52 && s[i] != letters[j]) { ++j; }
-		pos *= 10LL;
+        long long j = (int)s[i] - (int)'A';
+        pos *= 10LL;
 		pos += j;
-		pos %= length;
+		pos %= LENGTH;
 	}
 	pos %= 20923;
-	pos = (((pos * (static_cast<long long>(s.size()) % 10910)) % 1054094) * 1244) % length;
-	while (pos < length && table[pos].first != s && table[pos].first != "" && table[pos].first != (tomb == 1 ? "0" : "1")) {
+	pos = (((pos * (static_cast<long long>(s.size()) % 10910)) % 1054094) * 1244) % LENGTH;
+	while (pos < LENGTH && table[pos].first != s && table[pos].first != "" && table[pos].first != (tomb == 1 ? "0" : "1")) {
 		++pos;
-		if (pos == length) {
+		if (pos == LENGTH) {
 			pos *= 432;
 			pos += 97826;
-			pos %= (length + 124153);
-			pos %= length;
+			pos %= (LENGTH + 124153);
+			pos %= LENGTH;
 		}
 	}
 	return pos;
@@ -32,13 +30,11 @@ int main()
 {
 	std::ifstream in ("map.in");
 	std::ofstream out("map.out");
-	std::vector<std::pair<std::string, std::string>> table(length);
-	for (size_t i = 0; i < length; ++i) {
-		table[i].first = "";
-		table[i].second = "none";
-	}
+	std::vector<std::pair<std::string, std::string>> table(LENGTH, {"", "none"});
 
-	std::string s, key, value;
+	std::string s;
+	std::string key;
+	std::string value;
 	while (in >> s) {
 		in >> key;
 		if (s == "put") {
