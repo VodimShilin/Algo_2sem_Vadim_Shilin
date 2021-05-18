9918c82668e6d5aff28e4c53dd3bf3325c66d5b5
VodimShilin <VodimShilin>
2021-05-18 22:38:16 +0300

    add taskC

---
+++ Contest10/TaskC/main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+constexpr long long INF = 2'147'483'647;
+
+struct Edge{
+    Edge() = default;
+    Edge(long long x, long long y, long long w, long long index): x(x), y(y), w(w), index(index) {}
+    Edge(const Edge&) = default;
+    Edge(Edge&& another): x(another.x), y(another.y), w(another.w), index(another.index) {}
+    Edge reverse() {
+        std::swap(x, y);
+        return *this;
+    }
+    Edge& operator=(const Edge&) = default;
+
+    bool operator>(const Edge& another) const {
+        return w > another.w;
+    }
+    bool operator<(const Edge& another) const {
+        return w < another.w;
+    }
+    bool operator==(const Edge& another) const {
+        return w == another.w;
+    }
+
+    long long x;
+    long long y;
+    long long w;
+    long long index;
+};
+
+std::istream& operator>>(std::istream& in, Edge& e) {
+    in >> e.x >> e.y >> e.w;
+    --e.x;
+    --e.y;
+    return in;
+}
+
+//parents(n, -1) size(n, 1)
+class Graph{
+public:
+    Graph(long long n, std::vector<Edge>&& edge): n(n), m(edge.size()), edges(std::move(edge)), matrix_of_adjacency(n),
+                                                  parents(n, -1), size(n, 1) {
+        for (int i = 0; i < m; ++i) {
+            matrix_of_adjacency[edges[i].x].push_back(edges[i]);
+            matrix_of_adjacency[edges[i].y].push_back(edges[i].reverse());
+        }
+    }
+
+    long long Kruskal() {
+        std::sort(edges.begin(), edges.end());
+        long long sum = 0;
+        for (int i = 0; i < m; ++i) {
+            if (unite(edges[i].x, edges[i].y))
+                sum += edges[i].w;
+        }
+        return sum;
+    }
+
+    long long get(long long v) {
+        if (parents[v] == -1)
+            return v;
+        else
+            return parents[v] = get(parents[v]);
+    }
+
+    bool unite(long long u, long long v) {
+        u = get(u);
+        v = get(v);
+        if (u == v)
+            return false;
+        if (size[u] < size[v])
+            std::swap(u, v);
+        parents[v] = u;
+        size[u] += size[v];
+        return true;
+    }
+
+private:
+    long long n;
+    long long m;
+    std::vector<Edge> edges;
+    std::vector<std::vector<Edge>> matrix_of_adjacency;
+    std::vector<long long> parents;
+    std::vector<long long> size;
+    std::vector<std::vector<long long>> number_of_edge;
+    std::vector<std::vector<long long>> list_of_adjacency;
+    std::vector<std::vector<long long>> matrix_of_incidence;
+};
+
+int main() {
+    long long n, m;
+    std::cin >> n >> m;
+    std::vector<long long> a(n);
+    std::vector<Edge> edges(m + n - 1);
+    int min = 0;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+        if (a[i] < a[min])
+            min = i;
+    }
+    int c = 1;
+    for (int i = 0; i < n; ++i) {
+        if (min == i) {
+            c = 0;
+            continue;
+        }
+        edges[m + i + c - 1] = Edge(min, i, a[min] + a[i], m + i + c - 1);
+    }
+    for (int i = 0; i < m; ++i) {
+        std::cin >> edges[i];
+        edges[i].index = i;
+    }
+    Graph g = Graph(n, std::move(edges));
+    std::cout << g.Kruskal();
+    return 0;
+}
